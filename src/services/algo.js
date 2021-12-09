ce51517c6f39bb0e1b8fab7a2435b292d0c4d847
Ramin R <ramin.r9877@gmail.com>
2021-12-09 20:33:22 +0330

    algo service added

---
+++ src/services/algo.js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+
+const baseUrl = "https://algoexplorerapi.io/v2"
+
+async function getAlgoTransaction(hash) {
+    try {
+        const response = await axios.get(`${baseUrl}/transactions/pending/${hash}?format=json`)
+        if (response.status === 200 && response.data['confirmed-round'] > 0) {
+            return {
+                success: true,
+                data: {
+                    confirmed: true,
+                    receiver: response.data.txn.txn.arcv,
+                },
+            }
+        } else {
+            return {
+                success: true,
+                data: {
+                    confirmed: false,
+                }
+            }
+        }
+    } catch (e) {
+        // Treat not found (404) and bad request (400) as unconfirmed
+        if (e.response.status >= 400 && e.response.status < 500) {
+            return {
+                success: true,
+                data: {
+                    confirmed: false,
+                }
+            }
+        }
+
+        return {
+            success: false,
+            error: e,
+        }
+    }
+}
+
+module.exports = {
+    getAlgoTransaction,
+}
