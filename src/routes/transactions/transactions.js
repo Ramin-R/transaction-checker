0041c6c3799a3d43612a7439fae1ad492e60fc8a
Ramin R <ramin.r9877@gmail.com>
2021-12-09 23:18:25 +0330

    transaction handler completed

--- src/routes/transactions/transactions.js
+++ src/routes/transactions/transactions.js
@@ -1,13 +1,39 @@
 const express = require('express')
+const { getAlgoTransaction } = require('../../services/algo')
+const { getEtherOrBscTransaction } = require('../../services/ether')
+const { getOmniTransaction } = require('../../services/omni')
+const { getTronTransaction } = require('../../services/tron')
 
 const transactionsRouter = express.Router()
 
 transactionsRouter.get('/', transactionStatus)
 
 async function transactionStatus(req, res) {
-    return res.status(200).json({
-        status: "Ok"
-    })
+    const { hash, explorer } = req.query
+
+    let result
+
+    switch (explorer) {
+        case "algo":
+            result = await getAlgoTransaction(hash)
+            break
+        case "bsc":
+            result = await getEtherOrBscTransaction(hash, false)
+            break
+        case "ether":
+            result = await getEtherOrBscTransaction(hash, true)
+            break
+        case "omni":
+            result = await getOmniTransaction(hash)
+            break
+        case "tron":
+            result = await getTronTransaction(hash)
+            break
+        default:
+            result = { success: false, error: "Invalid or unsupported explorer" }
+    }
+
+    return res.json(result)
 }
 
 module.exports = transactionsRouter
